294fa556f0e9cd5d005fe8320c114d1f61fb8487
Andrei Faur <bogdan.andrei.faur@gmail.com>
2024-12-03 15:50:52 +0100

    About Page y retoques en Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import { useTranslation } from 'react-i18next';
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 import Landing from './pages/Landing.jsx';
+import About from './pages/About.jsx';
 import './App.css';
 
 const App = () => {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
         <html lang={i18n.language} />
+        <meta name="description" content={t('meta_description')} />
       </Helmet>
       <Router>
         <NavBar />
-        <div className="my-28">
+        <div className="mt-14 mb-28">
           <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
         <Footer />
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import NeuralNetworkAnimation from '../components/NeuralNetworkAnimation.jsx';
-import MindMap from "../components/MindMap.jsx";
+import MindMap from "../components/Landing/MindMap.jsx";
+import { getOptimizedUrl } from '../utils/cloudinary.js';
 
 const Landing = () => {
   const { t } = useTranslation();
@@ -10,23 +10,25 @@ const Landing = () => {
     <div className="flex flex-col justify-center items-center">
       <Helmet>
         <title>{t('landing.meta_title')}</title>
-        <meta name="description" content={t('landing.meta_description')} />
       </Helmet>
 
       <div className="flex flex-col w-full mt-10 items-center space-y-10 text-center">
-        <h1 className="w-96 lg:w-1/2 text-5xl lg:text-6xl font-semibold">
-          {t('landing.title')}
-        </h1>
-        <p className="w-96 lg:w-1/2 text-xl lg:text-3xl text-zinc-700 dark:text-zinc-300">
-          {t('landing.description')}
-        </p>
+        <div className='flex flex-col items-center space-y-8'>
+          <h1 className="w-96 lg:w-1/2 text-5xl lg:text-6xl font-semibold">
+            {t('landing.title')}
+          </h1>
+          
+          <p className="w-96 lg:w-1/2 text-xl lg:text-3xl text-zinc-700 dark:text-zinc-300">
+            {t('landing.description')}
+          </p>
+        </div>
 
         <a className="bg-indigo-600 bg-opacity-70 lg:hover:bg-opacity-100 text-white py-2 px-4 text-2xl font-semibold rounded-3xl transition-colors duration-500"
           href="/register"
         >{t('landing.get_started')}</a>
 
         <div className="mt-10">
-          <NeuralNetworkAnimation />
+          <img className='max-w-xs lg:max-w-xl' src={getOptimizedUrl("MindScan/undraw_medicine")} alt="Mental health" />
         </div>
 
         <MindMap />
