5d946cdc3557e273cdb0b5f12eb27e2f392db8ab
Andrei Faur <bogdan.andrei.faur@gmail.com>
2025-01-24 13:17:05 +0100

    Se ha añadido comprobación de Rol en ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,25 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUser, setUserFromToken } from "../../features/auth/loginUserSlice.js";
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({ children, requiredRole }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // Podemos leer el token de Redux (opcional) para ver si ya lo tenemos
   const { token } = useSelector((state) => state.loginUser);
 
   useEffect(() => {
-    // Si no tienes token en Redux, prueba leerlo de localStorage.
-    // (Tal vez Redux se vació al recargar la página.)
     const localToken = token || localStorage.getItem("token") || sessionStorage.getItem("token");
 
-    // Si no hay token ni en Redux ni en localStorage o sessionStorage, redirige a login.
     if (!localToken) {
       navigate("/login");
       return;
     }
 
-    // Verifica el token con el backend
     fetch("http://localhost:3001/auth/verify-user", {
       method: "POST",
       headers: {
@@ -31,24 +26,39 @@ export default function ProtectedRoute({ children }) {
     })
       .then(async (res) => {
         if (!res.ok) {
-          // Token inválido o caducado
           throw new Error("Invalid token");
         }
         return res.json();
       })
       .then((data) => {
-        // Guardar user y token en Redux
         dispatch(setUserFromToken({ user: data.user, token: localToken }));
+
+        if (Array.isArray(requiredRole)) {
+          if (!requiredRole.includes(data.user.role)) {
+            throw new Error("Unauthorized Role");
+          }
+        } else if (requiredRole && data.user.role !== requiredRole) {
+          throw new Error("Unauthorized Role");
+        }
       })
       .catch((error) => {
         console.error(error);
-        // Borrar token de localStorage, sessionStorage y Redux
-        localStorage.removeItem("token");
-        sessionStorage.removeItem("token");
-        dispatch(logoutUser());
-        navigate("/login");
+
+        if (error.message === "Invalid token") {
+          localStorage.removeItem("token");
+          sessionStorage.removeItem("token");
+          dispatch(logoutUser());
+          navigate("/login");
+        } else if (error.message === "Unauthorized Role") {
+          navigate("/");
+        } else {
+          localStorage.removeItem("token");
+          sessionStorage.removeItem("token");
+          dispatch(logoutUser());
+          navigate("/");
+        }
       });
-  }, [dispatch, navigate, token]);
+  }, [dispatch, navigate, token, requiredRole]);
 
   return children;
 }
